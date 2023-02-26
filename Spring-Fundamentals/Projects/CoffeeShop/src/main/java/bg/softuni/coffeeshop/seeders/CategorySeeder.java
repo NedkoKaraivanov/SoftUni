a91b7409b40507fe24280c07003926e64f8b588e
Nedko Karaivanov <nedko2016@gmail.com>
2023-02-26 23:42:54 +0200

    Changed Category Initialization

--- Spring-Fundamentals/Projects/CoffeeShop/src/main/java/bg/softuni/coffeeshop/seeders/CategorySeeder.java
+++ Spring-Fundamentals/Projects/CoffeeShop/src/main/java/bg/softuni/coffeeshop/seeders/CategorySeeder.java
@@ -10,7 +10,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-//@Component
+@Component
 public class CategorySeeder implements CommandLineRunner {
 
     private final CategoryRepository categoryRepository;
